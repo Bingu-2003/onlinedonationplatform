3dd401fbbcccac2862644f9a8d42a226d12f330b
Binguni <Binguni@172.16.102.65>
2024-11-20 19:45:59 +0530

    First Commit!

--- src/inf/login.java
+++ src/inf/login.java
@@ -4,6 +4,8 @@
  */
 package inf;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Binguni
@@ -26,55 +28,266 @@ public class login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollBar1 = new javax.swing.JScrollBar();
+        jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
+        jlblworldvision = new javax.swing.JLabel();
+        jlbllogintoaccount = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
+        jlblaccount = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jlblusername = new javax.swing.JLabel();
+        jlblpassword = new javax.swing.JLabel();
+        txtusername = new javax.swing.JTextField();
+        jpassword = new javax.swing.JPasswordField();
+        jbtnlogin = new javax.swing.JButton();
+        jbtngoogle = new javax.swing.JButton();
+        jlblforgot = new javax.swing.JLabel();
+        jbtni = new javax.swing.JButton();
+        jbtnfb = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(500, 400));
+        setPreferredSize(new java.awt.Dimension(800, 600));
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel1.setPreferredSize(new java.awt.Dimension(330, 410));
+        jPanel2.setBackground(new java.awt.Color(0, 51, 102));
+        jPanel2.setPreferredSize(new java.awt.Dimension(800, 600));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/charity.png"))); // NOI18N
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.setPreferredSize(new java.awt.Dimension(700, 500));
+
+        jlblworldvision.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 1, 18)); // NOI18N
+        jlblworldvision.setForeground(new java.awt.Color(0, 51, 102));
+        jlblworldvision.setText("WorldVision SriLanka");
+
+        jlbllogintoaccount.setFont(new java.awt.Font("Sitka Text", 1, 36)); // NOI18N
+        jlbllogintoaccount.setForeground(new java.awt.Color(0, 51, 102));
+        jlbllogintoaccount.setText("Login to Your Account");
+
+        jlblaccount.setBackground(new java.awt.Color(0, 51, 102));
+        jlblaccount.setFont(new java.awt.Font("Verdana Pro Cond Semibold", 1, 18)); // NOI18N
+        jlblaccount.setForeground(new java.awt.Color(102, 102, 102));
+        jlblaccount.setText("Login to your WorldVision Account... ");
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/money (1).png"))); // NOI18N
+
+        jlblusername.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
+        jlblusername.setText("Username");
+
+        jlblpassword.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
+        jlblpassword.setText("Password");
+
+        txtusername.setBackground(new java.awt.Color(204, 204, 204));
+        txtusername.setBorder(null);
+        txtusername.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtusernameActionPerformed(evt);
+            }
+        });
+
+        jpassword.setBackground(new java.awt.Color(204, 204, 204));
+        jpassword.setText("jPasswordField1");
+
+        jbtnlogin.setBackground(new java.awt.Color(204, 204, 204));
+        jbtnlogin.setFont(new java.awt.Font("Segoe UI", 1, 13)); // NOI18N
+        jbtnlogin.setText("LOGIN");
+        jbtnlogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnloginActionPerformed(evt);
+            }
+        });
+
+        jbtngoogle.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
+        jbtngoogle.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/circle-g_10490284 (1).png"))); // NOI18N
+        jbtngoogle.setText("Sign in with Google");
+        jbtngoogle.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
+        jbtngoogle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtngoogleActionPerformed(evt);
+            }
+        });
+
+        jlblforgot.setBackground(new java.awt.Color(204, 204, 204));
+        jlblforgot.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jlblforgot.setText("Forgot Password?");
+
+        jbtni.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
+        jbtni.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/aaz8nox3y.jpg"))); // NOI18N
+        jbtni.setText("Sign in with Apple Account");
+        jbtni.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
+        jbtni.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtniActionPerformed(evt);
+            }
+        });
+
+        jbtnfb.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
+        jbtnfb.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/facebook_1384005.png"))); // NOI18N
+        jbtnfb.setText("Sign in with Facebook");
+        jbtnfb.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 51), 2, true));
+        jbtnfb.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnfbActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(58, Short.MAX_VALUE))
+                .addGap(38, 38, 38)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jbtnlogin)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jlblforgot)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jlblpassword)
+                            .addComponent(jlblworldvision)
+                            .addComponent(jLabel2)
+                            .addComponent(txtusername, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jpassword, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(69, 69, 69)
+                                .addComponent(jLabel4))
+                            .addComponent(jlblusername))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(201, 201, 201)
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel1)
+                                .addContainerGap())
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 79, Short.MAX_VALUE)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jlbllogintoaccount, javax.swing.GroupLayout.PREFERRED_SIZE, 422, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(jlblaccount))
+                                        .addGap(24, 24, 24))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jbtni, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(jbtngoogle, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(jbtnfb, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGap(115, 115, 115))))))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(82, 82, 82)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(168, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(19, 19, 19)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jlblworldvision)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jlblusername))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(107, 107, 107)
+                        .addComponent(jlbllogintoaccount)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jlblaccount)
+                        .addGap(85, 85, 85)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(8, 8, 8)
+                        .addComponent(jbtngoogle, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jbtnfb, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jbtni, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(txtusername, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jlblpassword)
+                        .addGap(3, 3, 3)
+                        .addComponent(jpassword, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jbtnlogin)
+                            .addComponent(jlblforgot))
+                        .addGap(73, 73, 73))))
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-10, 0, 210, 350));
-
-        jPanel3.setBackground(new java.awt.Color(0, 153, 153));
-
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 330, Short.MAX_VALUE)
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 740, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(40, Short.MAX_VALUE))
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 350, Short.MAX_VALUE)
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(107, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 0, 330, 350));
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, -1));
 
-        pack();
+        setSize(new java.awt.Dimension(831, 559));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtusernameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtusernameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtusernameActionPerformed
+
+    private void jbtngoogleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtngoogleActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jbtngoogleActionPerformed
+
+    private void jbtniActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtniActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jbtniActionPerformed
+
+    private void jbtnfbActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnfbActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jbtnfbActionPerformed
+
+    private void jbtnloginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnloginActionPerformed
+       
+        String uname;
+        String pword;
+        
+        uname= txtusername.getText();
+         pword= jpassword.getText();
+        
+         if(uname.equals("Bingu") && pword.equals("2003")) {
+         
+          home home= new home();
+        home.setVisible(true);
+        this.dispose();
+         
+         
+         }else
+         {
+         JOptionPane.showMessageDialog(null,"Username or Password incorrect!");
+         }
+        
+        
+        
+       
+    }//GEN-LAST:event_jbtnloginActionPerformed
 
     /**
      * @param args the command line arguments
@@ -113,7 +326,23 @@ public class login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollBar jScrollBar1;
+    private javax.swing.JButton jbtnfb;
+    private javax.swing.JButton jbtngoogle;
+    private javax.swing.JButton jbtni;
+    private javax.swing.JButton jbtnlogin;
+    private javax.swing.JLabel jlblaccount;
+    private javax.swing.JLabel jlblforgot;
+    private javax.swing.JLabel jlbllogintoaccount;
+    private javax.swing.JLabel jlblpassword;
+    private javax.swing.JLabel jlblusername;
+    private javax.swing.JLabel jlblworldvision;
+    private javax.swing.JPasswordField jpassword;
+    private javax.swing.JTextField txtusername;
     // End of variables declaration//GEN-END:variables
 }
